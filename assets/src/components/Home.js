738998a438ebccba23f0c3d10b7bffa62c16922e
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2022-05-29 20:54:52 -0400

    created home page

---
+++ assets/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="text-center mt-3">
+            <h2>Welcome!</h2>
+            {/* add links to other pages here */}
+        </div>
+    )
+}
+
+export default Home;
