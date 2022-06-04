8722092868b9d17755feb28c98c11c6d38fa9e67
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2022-06-04 18:11:45 -0400

    added authpage and auth route

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from  "react-router-dom";
+import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
         <Router>
             <Routes>
                 <Route exact path="/" element={<HomePage/>} />
+                <Route exact path="/auth" element={<AuthPage/>} />
             </Routes>
         </Router>
     )
