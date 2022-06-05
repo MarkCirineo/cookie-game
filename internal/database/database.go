86393a21e96613126ea86f089beeef6d867c4acd
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2022-06-05 00:53:13 -0400

    refactored to use config file

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,29 +1,16 @@
 package database
 
 import (
-	"log"
-	"os"
-	"path/filepath"
-	"runtime"
+	"github.com/markcirineo/cookie-game/internal/conf"
 
 	"github.com/go-pg/pg/v10"
-	"github.com/joho/godotenv"
 )
 
-func NewDBOptions() *pg.Options {
-	_, b, _, _ := runtime.Caller(0)
-	basepath := filepath.Dir(b)
-	actualpath := basepath + "\\.env"
-	err := godotenv.Load(actualpath)
-	if err != nil {
-		log.Fatal("Error loading .env file", err)
-	}
-
-	password := os.Getenv("PSQL_PASSWORD")
+func NewDBOptions(cfg conf.Config) *pg.Options {
 	return &pg.Options{
-		Addr: "localhost:5432",
-		Database: "cookiegame",
-		User: "postgres",
-		Password: password,
+		Addr: cfg.DbHost + ":" +  cfg.DbPort,
+		Database: cfg.DbName,
+		User: cfg.DbUser,
+		Password: cfg.DbPassword,
 	}
 }
