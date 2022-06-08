3706036d888ff5eb5488d4a7e9d0ee8d9e8d2688
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2022-06-07 23:37:05 -0400

    added jwt generation to signup and signin functions

--- internal/server/user.go
+++ internal/server/user.go
@@ -22,7 +22,7 @@ func signUp(ctx *gin.Context) {
 	
 	ctx.JSON(http.StatusOK, gin.H{
 		"msg": "Signed up successfully.",
-		"jwt": "123456789",
+		"jwt": generateJWT(user),
 	})
 }
 
@@ -40,7 +40,7 @@ func signIn(ctx *gin.Context) {
 	
 	ctx.JSON(http.StatusOK, gin.H{
 		"msg": "Signed in successfully.",
-		"jwt": "123456789",
+		"jwt": generateJWT(user),
 	})
 
 	ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"err": "Sign in failed."})
