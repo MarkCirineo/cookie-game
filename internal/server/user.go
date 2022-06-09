35242e0942560f8be2c8f57a0e625ae24f94b1ef
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2022-06-08 22:58:42 -0400

    created handler for /api/cookies route

--- internal/server/user.go
+++ internal/server/user.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/markcirineo/cookie-game/internal/store"
 
@@ -44,4 +46,40 @@ func signIn(ctx *gin.Context) {
 	})
 
 	ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"err": "Sign in failed."})
+}
+
+func addCookies(ctx *gin.Context) {
+	update := new(store.UpdateCookies)
+	if err := ctx.Bind(update); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := currentUser(ctx)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if user.ID != update.UserID {
+		ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "not authorized"})
+		return
+	}
+	
+	if user.LastClaimed.Add(time.Minute * 30).After(time.Now()) {
+		ctx.AbortWithStatusJSON(http.StatusAccepted, gin.H{"msg": "time left on cooldown"})
+		return
+	}
+	randomCookies := rand.Intn(40) - 10
+	newCookies := randomCookies + user.Cookies
+
+	updatedUser, err := store.AddCookies(update.UserID, newCookies)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg": "successfully added cookies",
+		"data": updatedUser,
+	})
 }
