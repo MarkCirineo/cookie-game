6cbcb6e6ea8340837e3dc81b9bc472e80241d187
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2022-06-10 23:06:22 -0400

    added graceful shutdown to server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,18 @@
 package server
 
 import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/markcirineo/cookie-game/internal/conf"
 	"github.com/markcirineo/cookie-game/internal/database"
 	"github.com/markcirineo/cookie-game/internal/store"
+	"github.com/rs/zerolog/log"
 )
 
 const InternalServerError = "Something went wrong!"
@@ -15,5 +24,28 @@ func Start(cfg conf.Config) {
 
 	router := setRouter()
 
-	router.Run(":8080")
+	server := &http.Server{
+		Addr: cfg.Host + ":" + cfg.Port,
+		Handler: router,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			log.Error().Err(err).Msg("server ListenAndServe error")
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Info().Msg("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal().Err(err).Msg("server forced to shutdown")
+	}
+	
+	log.Info().Msg("server exiting")
 }
