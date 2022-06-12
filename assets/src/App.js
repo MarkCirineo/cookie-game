7019817594924ebfad03d40908e3c3e054a41f31
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2022-06-12 18:52:11 -0400

    added NavBar to App.js

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
     return (
         <Router>
+            <NavBar />
             <Routes>
                 <Route exact path="/" element={<HomePage />} />
                 <Route exact path="/auth" element={<AuthPage />} />
